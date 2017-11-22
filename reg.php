7e53937e5222f4ddb2fbcca7893ec26d92c8b644
Skipper <sas0367@gmail.com>
2017-11-22 22:31:35 +0300

    minor

--- reg.php
+++ reg.php
@@ -30,31 +30,31 @@
 </div>
     </div>
     <!-- #header -->
-
-<div class='container-fluid' >
+<center><h1 style="margin-top:80px">Регестрация</h1></center>
+<div class='container' style="margin-top:80px;" >
 <div class='row'>
-  <div class='col-lg-3 col-lg-offset-4' style="" style="margin:10px;" >
+  <div class='col-lg-4 col-lg-offset-4' style="background-color:rgb(51,51,51);" style="margin:10px;" >
   <form>
     <div class="form-group">
-      <label for="username">Ваше имя:</label>
-      <input type="text" class="form-control" id="username">
+      <label for="username">-</label>
+      <input type="text" class="form-control" required placeholder="Имя пользователя" id="username">
     </div>
    <div class="form-group">
-     <label for="email">Email:</label>
-     <input type="email" class="form-control" id="email">
+     <label for="email">-</label>
+     <input type="email" class="form-control" required placeholder="Email" id="email">
    </div>
    <div class="form-group">
-     <label for="pwd">Пароль:</label>
-     <input type="password" class="form-control" id="pwd">
+     <label for="pwd">-</label>
+     <input type="password" class="form-control" required placeholder="Пароль" id="pwd">
    </div>
    <div class="form-group">
-     <label for="pwdconf">Подтвердите пароль:</label>
-     <input type="password" class="form-control" id="pwdconf">
+     <label for="pwdconf">-</label>
+     <input type="password" class="form-control" required placeholder="Подтверждение пароля" id="pwdconf">
    </div>
    <div class="checkbox">
-     <label><input type="checkbox"> Я согласен с правилами пользования</label>
+     <label style="color:#FFFFFF;"><input type="checkbox"> Я согласен с правилами пользования</label>
    </div>
-   <button type="submit" class="btn btn-default">Подтвердить</button>
+   <button type="submit" name="button" class="btn btn-default">Подтвердить</button>
  </form>
 </div>
 </div>
